5f482d470f13a5d6638ef5c1e9a0806a101fa975
darkwolfie37 <79094301+darkwolfie37@users.noreply.github.com>
2022-10-10 10:53:55 -0400

    Adding YouTube Player Support.

--- index.php
+++ index.php
@@ -14,17 +14,17 @@
 <meta name="author" content="DarkWolfie">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 </head>
 <body>
     <div class="topbar">
         <div class="topbar-left">
-            <a href="index.php"><img src="images/logo.png" alt="WolfTube" width="124" height="32"></a>
+            <a href="index.php"><img src="images/logo.png" alt="WolfTube" width="124" height="28"></a>
         </div>
        
         <div class="topbar-center">
             <form action="search.php" method="GET">
-                <input type="text" name="query" width="510" height="30"> 
+                <input type="text" name="query" placeholder="Search"> <br>
                 <input type="submit" value="Search">   
             </form>
         </div>
