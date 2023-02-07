2309dafda9bf04ec712f36fd15ab2c92a36762af
darkwolfie37 <79094301+darkwolfie37@users.noreply.github.com>
2023-02-07 13:32:39 -0500

    AY

---
+++ checkuser.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD8p2wy9ftIXyYE_vj2xYKdkqQBc3K8e_I",
+    authDomain: "wolftube-6e83a.firebaseapp.com",
+    databaseURL: "https://wolftube-6e83a-default-rtdb.firebaseio.com",
+    projectId: "wolftube-6e83a",
+    storageBucket: "wolftube-6e83a.appspot.com",
+    messagingSenderId: "352393340794",
+    appId: "1:352393340794:web:9b9822a09c6667cc4a98c3",
+    measurementId: "G-X1W8K649YJ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
